c7fdde4db4719c9e2e0d0a55bc8ed5f15988afa8
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-03-11 13:14:05 +0900

    Update Program.cs

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -2,4 +2,12 @@ using System;
 using System.Collections.Generic;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+class DataProcessor
+{
+    public int[] numbers;
+
+    void ForEach(int[] array, Action<int> action)
+    {
+       
+    }
+}
